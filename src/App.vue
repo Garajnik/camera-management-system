e3b00cc4a80beaec3a85717b0346a3057530e239
Garajnik <lexan1699@gmail.com>
2025-07-16 20:12:05 +0700

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+    </a>
+  </div>
+  <HelloWorld/>
+</template>
+
+<style scoped>
+</style>
